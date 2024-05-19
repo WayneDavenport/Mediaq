65b8831f259f813b719f74631b6ae12357502676
Turtvonpickleshoe <111666555+Turtvonpickleshoe@users.noreply.github.com>
2024-05-19 09:37:41 -0500

    Removed websocket logic to refactor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,6 @@ import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    const socketInitializer = async () => {
-      await fetch('/api/socket');
-    };
-    socketInitializer();
-  }, []);
 
   return (
     <SessionProvider session={pageProps.session}>
