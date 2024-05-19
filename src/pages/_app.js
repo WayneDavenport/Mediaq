3471cdf538f8ca75cff46f7e16208c28aa79b448
Turtvonpickleshoe <111666555+Turtvonpickleshoe@users.noreply.github.com>
2024-05-18 21:33:56 -0500

    Basic C and R done, switched all DB logic to mongoose,fixed create/login bugs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,13 @@ import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const socketInitializer = async () => {
+      await fetch('/api/socket');
+    };
+    socketInitializer();
+  }, []);
+
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
